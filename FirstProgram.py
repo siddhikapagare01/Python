ef6621112b18712b6eb969447a1ababadb748de3
Siddhika Pagare <siddhikapagare01@gmail.com>
2024-12-29 20:59:14 +0530

    first Program

---
+++ FirstProgram.py
@@ -0,0 +1,44 @@
+print("Hello World..!")
+
+print("Siddhika is my name.")
+print("My age is 19")
+
+print("Siddhika is my name.", "My age is 19.")
+
+print("14")
+print("24")
+print(14+24)
+
+#Variable
+name = "Siddhika" #string
+age = 23 #integer
+price = 25.99 #float
+
+print("my name is:",name)
+print("my age is:",age)
+print("my price is:", price)
+age2 = age
+print(age2)
+
+print(type(name))
+print(type(age))
+print(type(price))
+
+#DataTypes - Int,str,float,Boolean,None
+name1 = "sp" #string
+name2 = 'sp'
+name3 = '''sp'''
+print(name1)
+print(name2)
+print(name3)
+
+age = 23 #Boolean
+old = False
+a = None
+print(type(old))
+print(type(a))
+
+a = 2 
+b = 5
+sum = a + b
+print(sum)

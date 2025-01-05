9190e1eac17a9f0c11a35718e3ac63f3369962d6
Siddhika Pagare <siddhikapagare01@gmail.com>
2025-01-05 13:44:58 +0530

    operators and input

---
+++ operators_input.py
@@ -0,0 +1,66 @@
+#relational operators
+'''a = 50
+b = 20
+ 
+print(a == b) #False
+print(a != b) #True
+print(a >= b) #Ture
+print(a > b) #True
+print(a <= b) #False
+print(a < b) #False
+
+#assignment operator
+num = 10
+#num = num + 10 #10+10 =>20
+num += 10  #(-= , *= , /= , **=)
+print("num :", num)
+
+#logical operator
+a = 50
+b = 30
+print(not False) #not 
+print(not (a > b))
+
+val1 = True
+val2 = False
+print("ans operator:", val1 and val2)
+
+print("or operator:", (a ==b) or (a > b))
+
+#type - coversion
+a = 2
+b = 4.25
+
+sum = a + b #2.0 + 4.25 => 6.25
+print(sum)
+
+# casting (manual)
+a = int("2")
+b = 4.25
+
+print(type(a))
+print(a + b)
+
+a = 3.14
+a = str(a)
+
+print(type(a))
+
+#input in python
+name = input("enter your name:")
+print("welcome" , name)
+
+name = input("enter your age:")
+print("You entered:" , name)
+
+int("5")
+val = int(input("enter some value:"))
+print(type(val), val)
+
+name = input("enter name:" )
+age = int(input("enter age: "))
+marks = float(input("enter marks:"))
+
+print("welcome", name)
+print("age =",age)
+print("marks =",marks)'''
